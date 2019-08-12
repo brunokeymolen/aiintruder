cb0fa8735b8b8f810cc6b03970a849de5b6fdf86
Bruno Keymolen <bruno.keymolen@gmail.com>
2019-08-12 21:01:32 +0200

    change notify

--- notify.sh
+++ notify.sh
@@ -3,6 +3,6 @@
 path=$1
 fname=$2
 
-echo -e "to: bruno.keymolen@gmail.com\nsubject: *Intruder*\n"| (cat - && uuencode $(path) $(fname)) | ssmtp bruno.keymolen@gmail.com
+echo -e "to: bruno.keymolen@gmail.com\nsubject: *Intruder*\n"| (cat - && uuencode ${path} ${fname}) | ssmtp bruno.keymolen@gmail.com
 
 
